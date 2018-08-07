c535e3ea06e7256bac8a7f8f720d77123d9a784e
Seifeddine Bouzid <bouzid.saif@hotmail.com>
2018-08-07 16:29:38 +0100

    "change database another time"

--- application/config/database.php
+++ application/config/database.php
@@ -79,7 +79,7 @@ $db['default'] = array(
 	'username' => 'root',
 	'password' => 'root1234',
 	'database' => 'singDB',
-	'dbdriver' => 'mysql',
+	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
 	'db_debug' => (ENVIRONMENT !== 'production'),
