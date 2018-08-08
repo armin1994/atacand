6e3af04f415f5ba7da962814c5f92bbc594c7553
Seifeddine Bouzid <bouzid.saif@hotmail.com>
2018-08-08 16:04:05 +0100

    "adding guzzlehttp_Fifthhattempt"

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -58,7 +58,7 @@ $autoload['packages'] = array();
 |
 |	$autoload['libraries'] = array('user_agent' => 'ua');
 */
-$autoload['libraries'] = array('database','session','upload','Alert','Guzzle');
+$autoload['libraries'] = array('database','session','upload','Alert');
 
 /*
 | -------------------------------------------------------------------
